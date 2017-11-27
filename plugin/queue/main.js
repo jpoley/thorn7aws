873d5568213dedc04e312bf89c477b29c247809d
chocopie116 <chocopie116@gmail.com>
2017-11-27 14:22:53 -0800

    .

---
+++ plugin/queue/main.js
@@ -0,0 +1,33 @@
+function dequeu() {
+}
+
+//return Promise
+function getS3ObjectByPath(path) {
+}
+
+function md5ByFile() {
+}
+
+function verifyMd5ByHttp() {
+}
+
+function archivedBatdImage() {
+}
+
+
+while (true) {
+    var p1 = dequeu();
+    var p2 = p1.then(function(result) {
+        getS3objectByPath
+    });
+
+
+
+
+    sleep(1);
+}
+
+//while
+
+//
+//
