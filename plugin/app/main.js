fcc602d147a94cc35b961f359c4a18ae3fe5ad0a
chocopie116 <chocopie116@gmail.com>
2017-11-27 20:19:31 -0800

    manage sqs message

--- plugin/app/main.js
+++ plugin/app/main.js
@@ -6,9 +6,7 @@ const getHashByS3Object = require('./tasks/getHash.js');
 const verifyHash = require('./tasks/verifyHash.js');
 const processFile = require('./tasks/processFile.js');
 
-const SQS_DEQUE_INTERVAL_SEC = 2000;
-
-function process(bucketName, objectKey) {
+function process(bucketName, objectKey, callback) {
     getHashByS3Object(bucketName, objectKey)
         .then(function(hash) {
             return verifyHash(hash);
@@ -24,6 +22,8 @@ function process(bucketName, objectKey) {
         }).catch(function(err) {
             return console.error('failied reason => ', err);
         });
+
+    return callback();
 }
 
 
@@ -32,24 +32,33 @@ function main() {
     const app = Consumer.create({
         queueUrl: 'https://sqs.us-east-1.amazonaws.com/561275206473/thorn_scale', /* required */
         handleMessage: (message, done) => {
-            console.log(message);
+            var body = JSON.parse(message.Body);
+            var message = JSON.parse(body.Message);
+            var record = message.Records[0];
 
-            //do some work with `message`
-            var bucketName = 'thorn7plugin';
-            var objectKey = 'kid.jpeg';
+            var bucketName =  record.s3.bucket.name;
+            var objectKey = record.s3.object.key;
 
-            process(bucketName, objectKey);
-            done();
+            if (objectKey.indexOf('matche') === 0) {
+                //skip uploaded
+                return done();
+            }
+
+            console.log(bucketName, objectKey);
+            //TODO remove hard coding
+            //var bucketName = 'thorn7plugin';
+            //var objectKey = 'kid.jpeg';
+
+            console.log('process');
+            process(bucketName, objectKey, done);
         }
     });
 
     app.start();
 }
 
+//var bucketName = 'thorn7plugin';
+//var objectKey = 'kid.jpeg';
+//process(bucketName, objectKey);
 
-var bucketName = 'thorn7plugin';
-var objectKey = 'kid.jpeg';
-process(bucketName, objectKey);
-
-//TODO switch if production
-//main();
+main();
