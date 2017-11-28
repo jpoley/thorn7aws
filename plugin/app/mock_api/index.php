f48ceddb99ce7d79ca6937766ffe87d5bfd1748e
chocopie116 <chocopie116@gmail.com>
2017-11-27 16:24:26 -0800

    modify mock api response format

--- plugin/app/mock_api/index.php
+++ plugin/app/mock_api/index.php
@@ -1,10 +1,21 @@
 <?php
 
-$results = ['result' =>
-    [
+$results = [
+    'result' => [
+        'input' => 'AAMACwAJAAgaNw1IKQMDPgcAABIGAAALEC4RWQ4cBIwdVzi8UDMbqU8FBoQVAAcSBqQ9KBWRHok7PU+5Rk8zkKEWIK+HAQJ5AKYULhCSGzhCHUgwPyJuLCMwQSi+BQwxAYhXBxfNVR6OFlEiHCihJhxbYC3bDykhCD4jARTjngjCDLIOOyN/Ah6azQqkDKcC',
         'match' => 1,
-        'level' => 'high',
-        'providers' => 'google'
+        'confidence' => 'high',
+        'score' => 33,
+        'details' => [
+            [
+                'vendor' => 'google',
+                'severity' => 'A1',
+            ],
+            [
+                'vendor' => 'microsoft',
+                'severity' => 'A2',
+            ],
+        ]
     ]
 ];
 
